3ba0bf0bbfcacade2a3b31435d74aaecdc70410b
lordmoon <1418015106@qq.com>
2025-07-15 23:31:39 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 // @ts-check
 
-import eslint from '@eslint/js';
+import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import stylistic from "@stylistic/eslint-plugin";
 
 export default tseslint.config(
+  {
+    ignores: ["node_modules", "dist"],
+  },
   eslint.configs.recommended,
   tseslint.configs.recommended,
   stylistic.configs.customize({
