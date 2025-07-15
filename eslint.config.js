0b83c6db2931afddf0073afe032abf283c5982f8
kevinzxfeng <kevinzxfeng@tencent.com>
2025-07-15 17:24:44 +0800

    chore: configuration add tsup and eslint and tsconfig setup building project

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  stylistic.configs.customize({
+    semi: true,
+    quotes: "double",
+  }),
+);
---
+++ tsup.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  outDir: "dist",
+  format: ["esm", "cjs"],
+  target: "es2020",
+});
